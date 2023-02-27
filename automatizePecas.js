a0c6e5e53ca07be03bd9a2b7a650bf0202265d06
Kzuy1 <leoczuy@gmail.com>
2023-02-26 21:59:24 -0300

    v0.5

--- automatizePecas.js
+++ automatizePecas.js
@@ -1,5 +1,5 @@
 const ExcelJS = require('exceljs');
-const filename = "./2023-02-15_LISTA MATERIAL - C0011_00_CHLOE_LES.xlsx";
+const filename = "./2023-02-15_LISTA MATERIAL - C0011_00_CHLOE_LES - Copy.xlsx";
 const codigoDeProjeto = "C122005";
 
 const workbook = new ExcelJS.Workbook();
@@ -7,7 +7,23 @@ const workbook = new ExcelJS.Workbook();
 async function f1() {
     await workbook.xlsx.readFile(filename).then(async function () {
         let sourceWorksheet = workbook.getWorksheet(1);
-        workbook.addWorksheet('Teste')
+
+        //Le os códigos e cria planilhas correspondentes
+        const colDesenho = sourceWorksheet.getColumn(1);
+        let desenhoCodigo = [];
+        colDesenho.eachCell(function(cell, rowNumber){
+            if((!desenhoCodigo.includes(cell.value)) && isNaN(cell.value) && cell.value != "DESENHO"){
+                desenhoCodigo.push(cell.value);
+            };
+        });
+
+        //Cria as planilhas
+        for(i = 0; i < desenhoCodigo.length; i++){
+            workbook.addWorksheet(`${desenhoCodigo[i]} - PEÇAS`);
+            workbook.addWorksheet(`${desenhoCodigo[i]} - MATERIAL`);
+        };
+
+
 
         workbook.xlsx.writeFile(`${filename}`);
     });
