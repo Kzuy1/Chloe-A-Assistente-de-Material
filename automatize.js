dfef1012c54402f5a1b8e13bb233dd352dbce459
Kzuy1 <leoczuy@gmail.com>
2023-03-05 23:07:36 -0300

    v0.7

--- automatize.js
+++ automatize.js
@@ -1,208 +1,208 @@
 const ExcelJS = require('exceljs');
-const {findMaterial, findMaterialEsp} = require('./findMaterial.js');
-const {adjustMaterial} = require('./adjustMaterial.js')
-const filename = "./2023-02-15_LISTA MATERIAL - C0011_00.xlsx";
+const { findMaterial, findMaterialEsp } = require('./findMaterial.js');
+const { adjustMaterial } = require('./adjustMaterial.js')
+const filename = "./2023-02-15_LISTA MATERIAL - C0011_00 - Copia.xlsx";
 const codigoDeProjeto = "C122005";
 
 const workbook = new ExcelJS.Workbook();
 
 async function f1() {
-    await workbook.xlsx.readFile(filename).then(async function () {
-        let sourceWorksheet = workbook.getWorksheet(1);
-        let targetWorkbook = new ExcelJS.Workbook();
-        let targetSheet = targetWorkbook.addWorksheet(sourceWorksheet.name);
-        targetSheet.model = Object.assign(sourceWorksheet.model,{
-            views:[{state: 'frozen',ySplit:1}],
-        });
-
-        targetSheet.spliceColumns(1, 0, ["DESENHO"], ["PEÇA"])  ;
-
-        //Adiciona coluna Desenho e Peça
-        for (rowIndex = 1; rowIndex <= targetSheet.rowCount; rowIndex++) {
-            targetSheet.getRow(rowIndex).alignment = { wrapText: false };
-        };
-
-        //Verifica Index para Adicionar linhas
-        const itemCol = targetSheet.getColumn(3);
-        let rowAdd = [];
-        itemCol.eachCell(function(cell, rowNumber) {
-            if(cell.value == null){
-                console.log(`🦋 Error CHX: Index vazio ${cell.address}`);
-                cell.style = {
-                    fill: { type: 'pattern', pattern:'solid',fgColor:{argb:'6ddd35'}},
-                };
-            } else if(!(cell.value.includes(".") || cell.value.includes("ITEM") || cell.value == "1")){
-                rowAdd.push(rowNumber);
-            }
-        });
-
-        //Adiciona uma linha em cima
-        for(i = 0; i < rowAdd.length; i++){
-            targetSheet.insertRow(rowAdd[i]+i);
-
-        }
-
-        //Move do Estoque Perfil para outra coluna de material
-        const estoqueCol = targetSheet.getColumn(6);
-        estoqueCol.eachCell(function(cell, rowNumber) {
-            if(cell.value != null){
-                targetSheet.getCell(`G${rowNumber}`).value = cell.value;
-            };
-
-        });
-
-        //Procura o material e substitui
-        const estoqueCol2 = targetSheet.getColumn(7);
-        estoqueCol2.eachCell(function(cell, rowNumber) {
-            if(cell.value == null){
-                if(targetSheet.getCell(`H${rowNumber}`).value != "Generic"){
-                    console.log(`🦋 Error CHX: Matérial não encontrado ${cell.address}`);
-                    cell.style = {
-                        fill: { type: 'pattern', pattern:'solid',fgColor:{argb:'6ddd35'}},
-                    };
-                }
-            } else if(!(cell.value.includes("NÚMERO DE ESTOQUE") || cell.value.includes("Descrição"))) {
-                cell.value = cell.value.replace(/^\s+|\s+$/g, "");
-                let material;
-                let tipoMaterial = targetSheet.getCell(`H${rowNumber}`);
-                if(tipoMaterial.value != "ASTM A36" && tipoMaterial.value != "ASTM A572 Gr. 50") {
-                    console.log(`🚨 Alerta: Material diferente de Aço Carbono ${cell.address}`);
-                    material = findMaterialEsp(cell.value, tipoMaterial);
-                } else {
-                    material = findMaterial(cell.value);
-                };
-
-                if (material == undefined){
-                    console.log(`🦋 Error CHX: Matérial não cadastrado ${cell.address}`);
-                    cell.style = {
-                        fill: { type: 'pattern', pattern:'solid',fgColor:{argb:'6ddd35'}},
-                    };
-                } else {
-                    cell.value = material.descricao;
-                    targetSheet.getCell(`H${rowNumber}`).value = material.material;
-                }
-
-            };
-
-        });
-        
-        //Verifica o Peso
-        const massaCol = targetSheet.getColumn(9);
-        massaCol.eachCell(function(cell, rowNumber) {
-            if(cell.value == null){
-                console.log(`🦋 Error CHX: Peso não encontrado ${cell.address}`);
-                cell.style = {
-                    fill: { type: 'pattern', pattern:'solid',fgColor:{argb:'6ddd35'}},
-                };
-            }else if(cell.value.includes("kg")){
-                cell.value = cell.value.replace(" kg", "").replace(",",".");
-                cell.value = Number(cell.value);
-            } else if(cell.value.includes("lb_massa")){
-                console.log(`🦋 Error CHX: Massa com Libra em ${cell.address}`);
-                cell.style = {
-                    fill: { type: 'pattern', pattern:'solid',fgColor:{argb:'F08080'}},
-                };
-            }
-        });
-
-        //Verifica tem Descrição dentro de uma peça
-        const estoqueCol21 = targetSheet.getColumn(7);
-        estoqueCol21.eachCell(function(cell, rowNumber) {
-            if((cell.value != null) && (cell.value.includes("Descrição"))){
-                if(targetSheet.getCell(`C${rowNumber}`).value.includes(".")){
-                    console.log(`🦋 Error CHX: Material com Descrição no Nome ${cell.address}`);
-                    cell.style = {
-                        fill: { type: 'pattern', pattern:'solid',fgColor:{argb:'F5D033'}},
-                    };
-                };
-            };
-
-        });
-
-        //Adiciona as formulas e colhe o material para colocar nas peças
-        const itemColUpdate = targetSheet.getColumn(3);
-        let localizePontos = [];
-        itemColUpdate.eachCell(function(cell, rowNumber){
-            if((cell.value != null) && (!cell.value.includes(".")) && (cell.value != "ITEM")){
-                let result = targetSheet.getCell(`D${rowNumber}`) * targetSheet.getCell(`I${rowNumber}`);
-                targetSheet.getCell(`K${rowNumber}`).value = {formula: `D${rowNumber}*I${rowNumber}`, result};
-
-                localizePontos.push({adress: rowNumber, index: cell.value, pesoMaterial: 0, material: []})
-            } else if((cell.value != null) && (cell.value.includes(".")) && (cell.value != "ITEM")){
-                let conjuntoFind = localizePontos.find(({index}) => index === cell.value.split('.')[0]);
-                let conjuntoIndex = localizePontos.findIndex(({index}) => index === cell.value.split('.')[0]);
-
-                let result = targetSheet.getCell(`D${rowNumber}`) * targetSheet.getCell(`I${rowNumber}`) * targetSheet.getCell(`D${conjuntoFind.adress}`);
-                targetSheet.getCell(`K${rowNumber}`).value = {formula: `D${rowNumber}*I${rowNumber}*$D$${conjuntoFind.adress}`, result};
-
-                let material = targetSheet.getCell(`H${rowNumber}`);
-                localizePontos[conjuntoIndex].material.push(material.value);
-                localizePontos[conjuntoIndex].pesoMaterial += result;
-            }
-
-        });
-        
-        //Adiciona os matériais as Peças
-        for(i = 0; i < localizePontos.length; i++){
-            if(localizePontos[i].material != false){
-                let newMaterial = adjustMaterial(localizePontos[i].material);
-                targetSheet.getCell(`H${localizePontos[i].adress}`).value = newMaterial;
-                
-                let infoPeca = targetSheet.getCell(`K${localizePontos[i].adress}`)
-                let pesoPeca = infoPeca.value.result
-
-                if (localizePontos[i].pesoMaterial > (pesoPeca + 0.1) || localizePontos[i].pesoMaterial < (pesoPeca - 0.1) || isNaN(localizePontos[i].pesoMaterial)){
-                    console.log(`🦋 Error CHX: Peso da Peça não batendo com soma dos Componentes ${infoPeca.address}`);
-                    infoPeca.style = {
-                        fill: { type: 'pattern', pattern:'solid',fgColor:{argb:'7F7679'}},
-                    };
-
-                };
-            };
-        };
-
-        //Verifica se a peça tem código
-        const numberoPeca = targetSheet.getColumn(5);
-            numberoPeca.eachCell(function(cell, rowNumber) {
-            if((cell.value != null) && (!cell.value.includes(codigoDeProjeto)) && (cell.value != "NÚMERO DA PEÇA")){
-                console.log(`🦋 Error CHX: Peça sem código ${cell.address}`);
-                cell.style = {
-                    fill: { type: 'pattern', pattern:'solid',fgColor:{argb:'CB2821'}},
-                };
-            };
-
-        });
-        
-        const qtdeBaseCol = targetSheet.getColumn(10);
-        qtdeBaseCol.hidden = true;
-
-        targetSheet.autoFilter = {
-            from: {
-              row: 1,
-              column: 1
-            },
-            to: {
-              row: targetSheet.lastRow.number,
-              column: 9
-            }
-        }
-
-        //Ajeita o Tamanho das colunas
-        targetSheet.columns.forEach(function (column, i) {
-            let maxLength = 0;
-            column["eachCell"]({ includeEmpty: false }, function (cell) {
-                var columnLength = cell.value ? cell.value.toString().length : 10;
-                if (columnLength > maxLength ) {
-                    maxLength = columnLength;
-                }
-            });
-            column.width = maxLength < 10 ? 10 : maxLength;
-        });
-
-        targetWorkbook.xlsx.writeFile(`${filename.replace(".xlsx", "")}_CHLOE_LES.xlsx`);
-        
+  await workbook.xlsx.readFile(filename).then(async function() {
+    let sourceWorksheet = workbook.getWorksheet(1);
+    let targetWorkbook = new ExcelJS.Workbook();
+    let targetSheet = targetWorkbook.addWorksheet(sourceWorksheet.name);
+    targetSheet.model = Object.assign(sourceWorksheet.model, {
+      views: [{ state: 'frozen', ySplit: 1 }],
     });
-    
+
+    targetSheet.spliceColumns(1, 0, ["DESENHO"], ["PEÇA"]);
+
+    //Adiciona coluna Desenho e Peça
+    for (rowIndex = 1; rowIndex <= targetSheet.rowCount; rowIndex++) {
+      targetSheet.getRow(rowIndex).alignment = { wrapText: false };
+    };
+
+    //Verifica Index para Adicionar linhas
+    const itemCol = targetSheet.getColumn(3);
+    let rowAdd = [];
+    itemCol.eachCell(function(cell, rowNumber) {
+      if (cell.value == null) {
+        console.log(`🦋 Error CHX: Index vazio ${cell.address}`);
+        cell.style = {
+          fill: { type: 'pattern', pattern: 'solid', fgColor: { argb: '6ddd35' } },
+        };
+      } else if (!(cell.value.includes(".") || cell.value.includes("ITEM") || cell.value == "1")) {
+        rowAdd.push(rowNumber);
+      }
+    });
+
+    //Adiciona uma linha em cima
+    for (i = 0; i < rowAdd.length; i++) {
+      targetSheet.insertRow(rowAdd[i] + i);
+
+    }
+
+    //Move do Estoque Perfil para outra coluna de material
+    const estoqueCol = targetSheet.getColumn(6);
+    estoqueCol.eachCell(function(cell, rowNumber) {
+      if (cell.value != null) {
+        targetSheet.getCell(`G${rowNumber}`).value = cell.value;
+      };
+
+    });
+
+    //Procura o material e substitui
+    const estoqueCol2 = targetSheet.getColumn(7);
+    estoqueCol2.eachCell(function(cell, rowNumber) {
+      if (cell.value == null) {
+        if (targetSheet.getCell(`H${rowNumber}`).value != "Generic") {
+          console.log(`🦋 Error CHX: Matérial não encontrado ${cell.address}`);
+          cell.style = {
+            fill: { type: 'pattern', pattern: 'solid', fgColor: { argb: '6ddd35' } },
+          };
+        }
+      } else if (!(cell.value.includes("NÚMERO DE ESTOQUE") || cell.value.includes("Descrição"))) {
+        cell.value = cell.value.replace(/^\s+|\s+$/g, "");
+        let material;
+        let tipoMaterial = targetSheet.getCell(`H${rowNumber}`);
+        if (tipoMaterial.value != "ASTM A36" && tipoMaterial.value != "ASTM A572 Gr. 50") {
+          console.log(`🚨 Alerta: Material diferente de Aço Carbono ${cell.address}`);
+          material = findMaterialEsp(cell.value, tipoMaterial);
+        } else {
+          material = findMaterial(cell.value);
+        };
+
+        if (material == undefined) {
+          console.log(`🦋 Error CHX: Matérial não cadastrado ${cell.address}`);
+          cell.style = {
+            fill: { type: 'pattern', pattern: 'solid', fgColor: { argb: '6ddd35' } },
+          };
+        } else {
+          cell.value = material.descricao;
+          targetSheet.getCell(`H${rowNumber}`).value = material.material;
+        }
+
+      };
+
+    });
+
+    //Verifica o Peso
+    const massaCol = targetSheet.getColumn(9);
+    massaCol.eachCell(function(cell, rowNumber) {
+      if (cell.value == null) {
+        console.log(`🦋 Error CHX: Peso não encontrado ${cell.address}`);
+        cell.style = {
+          fill: { type: 'pattern', pattern: 'solid', fgColor: { argb: '6ddd35' } },
+        };
+      } else if (cell.value.includes("kg")) {
+        cell.value = cell.value.replace(" kg", "").replace(",", ".");
+        cell.value = Number(cell.value);
+      } else if (cell.value.includes("lb_massa")) {
+        console.log(`🦋 Error CHX: Massa com Libra em ${cell.address}`);
+        cell.style = {
+          fill: { type: 'pattern', pattern: 'solid', fgColor: { argb: 'F08080' } },
+        };
+      }
+    });
+
+    //Verifica tem Descrição dentro de uma peça
+    const estoqueCol21 = targetSheet.getColumn(7);
+    estoqueCol21.eachCell(function(cell, rowNumber) {
+      if ((cell.value != null) && (cell.value.includes("Descrição"))) {
+        if (targetSheet.getCell(`C${rowNumber}`).value.includes(".")) {
+          console.log(`🦋 Error CHX: Material com Descrição no Nome ${cell.address}`);
+          cell.style = {
+            fill: { type: 'pattern', pattern: 'solid', fgColor: { argb: 'F5D033' } },
+          };
+        };
+      };
+
+    });
+
+    //Adiciona as formulas e colhe o material para colocar nas peças
+    const itemColUpdate = targetSheet.getColumn(3);
+    let localizePontos = [];
+    itemColUpdate.eachCell(function(cell, rowNumber) {
+      if ((cell.value != null) && (!cell.value.includes(".")) && (cell.value != "ITEM")) {
+        let result = targetSheet.getCell(`D${rowNumber}`) * targetSheet.getCell(`I${rowNumber}`);
+        targetSheet.getCell(`K${rowNumber}`).value = { formula: `D${rowNumber}*I${rowNumber}`, result };
+
+        localizePontos.push({ adress: rowNumber, index: cell.value, pesoMaterial: 0, material: [] })
+      } else if ((cell.value != null) && (cell.value.includes(".")) && (cell.value != "ITEM")) {
+        let conjuntoFind = localizePontos.find(({ index }) => index === cell.value.split('.')[0]);
+        let conjuntoIndex = localizePontos.findIndex(({ index }) => index === cell.value.split('.')[0]);
+
+        let result = targetSheet.getCell(`D${rowNumber}`) * targetSheet.getCell(`I${rowNumber}`) * targetSheet.getCell(`D${conjuntoFind.adress}`);
+        targetSheet.getCell(`K${rowNumber}`).value = { formula: `D${rowNumber}*I${rowNumber}*$D$${conjuntoFind.adress}`, result };
+
+        let material = targetSheet.getCell(`H${rowNumber}`);
+        localizePontos[conjuntoIndex].material.push(material.value);
+        localizePontos[conjuntoIndex].pesoMaterial += result;
+      }
+
+    });
+
+    //Adiciona os matériais as Peças
+    for (i = 0; i < localizePontos.length; i++) {
+      if (localizePontos[i].material != false) {
+        let newMaterial = adjustMaterial(localizePontos[i].material);
+        targetSheet.getCell(`H${localizePontos[i].adress}`).value = newMaterial;
+
+        let infoPeca = targetSheet.getCell(`K${localizePontos[i].adress}`)
+        let pesoPeca = infoPeca.value.result
+
+        if (localizePontos[i].pesoMaterial > (pesoPeca + 0.1) || localizePontos[i].pesoMaterial < (pesoPeca - 0.1) || isNaN(localizePontos[i].pesoMaterial)) {
+          console.log(`🦋 Error CHX: Peso da Peça não batendo com soma dos Componentes ${infoPeca.address}`);
+          infoPeca.style = {
+            fill: { type: 'pattern', pattern: 'solid', fgColor: { argb: '7F7679' } },
+          };
+
+        };
+      };
+    };
+
+    //Verifica se a peça tem código
+    const numberoPeca = targetSheet.getColumn(5);
+    numberoPeca.eachCell(function(cell, rowNumber) {
+      if ((cell.value != null) && (!cell.value.includes(codigoDeProjeto)) && (cell.value != "NÚMERO DA PEÇA")) {
+        console.log(`🦋 Error CHX: Peça sem código ${cell.address}`);
+        cell.style = {
+          fill: { type: 'pattern', pattern: 'solid', fgColor: { argb: 'CB2821' } },
+        };
+      };
+
+    });
+
+    const qtdeBaseCol = targetSheet.getColumn(10);
+    qtdeBaseCol.hidden = true;
+
+    targetSheet.autoFilter = {
+      from: {
+        row: 1,
+        column: 1
+      },
+      to: {
+        row: targetSheet.lastRow.number,
+        column: 9
+      }
+    }
+
+    //Ajeita o Tamanho das colunas
+    targetSheet.columns.forEach(function(column, i) {
+      let maxLength = 0;
+      column["eachCell"]({ includeEmpty: false }, function(cell) {
+        var columnLength = cell.value ? cell.value.toString().length : 10;
+        if (columnLength > maxLength) {
+          maxLength = columnLength;
+        }
+      });
+      column.width = maxLength < 10 ? 10 : maxLength;
+    });
+
+    targetWorkbook.xlsx.writeFile(`${filename.replace(".xlsx", "")}_CHLOE_LES.xlsx`);
+
+  });
+
 };
 
 f1();
--- findMaterial.js
+++ findMaterial.js
@@ -1,59 +1,61 @@
-const findMaterial = (material) => {
-    let listMaterial = [
-        //CHAPAS MM
-        {modelo: "SHEET TH. 2,000 mm", descricao: "CHAPA ESP. 2.0mm", material: "ASTM A-36"},
-        //CHAPAS POL
-        {modelo: "SHEET TH. 4,763 mm", descricao: "CHAPA 3/16\"", material: "ASTM A-36"},
-        {modelo: "SHEET TH. 6,350 mm", descricao: "CHAPA 1/4\"", material: "ASTM A-36"},
-        {modelo: "SHEET TH. 7,938 mm", descricao: "CHAPA 5/16\"", material: "ASTM A-36"},
-        {modelo: "SHEET TH. 9,525 mm", descricao: "CHAPA 3/8\"", material: "ASTM A-36"},
-        {modelo: "SHEET TH. 12,700 mm", descricao: "CHAPA 1/2\"", material: "ASTM A-36"},
-        {modelo: "SHEET TH. 15,875 mm", descricao: "CHAPA 5/8\"", material: "ASTM A-36"},
-        {modelo: "SHEET TH. 19,050 mm", descricao: "CHAPA 3/4\"", material: "ASTM A-36"},
-        {modelo: "SHEET TH. 25,400 mm", descricao: "CHAPA 1\"", material: "ASTM A-36"},
-        {modelo: "SHEET TH. 31,750 mm", descricao: "CHAPA 1.1/4\"", material: "ASTM A-36"},
-        {modelo: "SHEET TH. 50,800 mm", descricao: "CHAPA 2\"", material: "ASTM A-36"},
-        //PERFIL L
-        {modelo: "L 2 x 2 x 3/16", descricao: "L 2\" x 3/16\"", material: "ASTM A-36"},
-        {modelo: "L 2 x 2 x 1/4", descricao: "L 2\" x 1/4\"", material: "ASTM A-36"},
-        {modelo: "L 2 1/2 x 2 1/2 x 3/16", descricao: "L 2.1/2\" x 3/16\"", material: "ASTM A-36"},
-        {modelo: "L 3 x 3 x 3/16", descricao: "L 3\" x 3/16\"", material: "ASTM A-36"},
-        {modelo: "L 3 x 3 x 1/4", descricao: "L 3\" x 1/4\"", material: "ASTM A-36"},
-        {modelo: "L 4 x 4 x 5/16", descricao: "L 4\" x 5/16\"", material: "ASTM A-36"},
-        //PERFIL U
-        {modelo: "C 4x5.4", descricao: "PERFIL U 4\" - 1ªALMA", material: "ASTM A-36"},
-        {modelo: "C 6x8.2", descricao: "PERFIL U 6\" - 1ªALMA", material: ""},
-        {modelo: "C 6 x 8,2", descricao: "PERFIL U 6\" - 1ªALMA", material: "ASTM A-36"},
-        {modelo: "C 8 x 11,5", descricao: "PERFIL U 8\" - 1ªALMA", material: "ASTM A-36"},
-        {modelo: "C 8x11.5", descricao: "PERFIL U 8\" - 1ªALMA", material: "ASTM A-36"},
-        //TUBOS
-        {modelo: "PIPE ØEXT. 33,400 mmx3,380 mm", descricao: "TUBO DN 1\" SCH40", material: "ASTM A53 Gr.B"},
-        {modelo: "PIPE ØEXT. 42,200 mmx3,560 mm", descricao: "TUBO DN 1.1/4\" SCH40", material: "ASTM A53 Gr.B"},
-        //PERFIL W
-        {modelo: "VIGA W 203,000 mmx102,000 mm", descricao: "W 200X19.3", material: "ASTM A572 Gr. 50"},
-        {modelo: "W150 x 22.5", descricao: "W 150x22.5", material: "ASTM A572 Gr. 50"},
-        {modelo: "W200 x 26.6", descricao: "W 200x26.6", material: "ASTM A572 Gr. 50"},
-        {modelo: "W200 x 46.1", descricao: "W 200x46,1", material: "ASTM A572 Gr. 50"},
-        {modelo: "W200 x 52", descricao: "W 200x52", material: "ASTM A572 Gr. 50"},
-        {modelo: "W250 x 32.7", descricao: "W 200x32.7", material: "ASTM A572 Gr. 50"},
-        //CURVAS
-        {modelo: "CURVA 90º DN 1\" SCH40 CFME ANSI B16.9 RC", descricao: "CURVA 90º DN 1\" SCH40 CFME ANSI B16.9 RC", material: "ASTM A-36"},
-        {modelo: "CURVA 90º DN 1.1/4\" SCH40 CFME ANSI B16.9 RC", descricao: "CURVA 90º DN 1.1/4\" SCH40 CFME ANSI B16.9 RC", material: "ASTM A-36"},
-        //{modelo: "", descricao: "", material: ""},
-    ] 
-
-    let info = listMaterial.filter(obj => obj.modelo == material);
+let listMaterial = [
+    //CHAPAS MM
+    { modelo: "SHEET TH. 2,000 mm",  materialMod: "", descricao: "CHAPA ESP. 2.0mm", unidade: "m²", quantidade: 0, material: "ASTM A-36", peso: 0},
+    //CHAPAS POL
+    { modelo: "SHEET TH. 4,763 mm",  materialMod: "", descricao: "CHAPA 3/16\"", unidade: "m²", quantidade: 0, material: "ASTM A-36", peso: 0},
+    { modelo: "SHEET TH. 6,350 mm",  materialMod: "", descricao: "CHAPA 1/4\"", unidade: "m²", quantidade: 0, material: "ASTM A-36", peso: 0},
+    { modelo: "SHEET TH. 6,350 mm",  materialMod: "USI SAC 350", descricao: "CHAPA 1/4\"", unidade: "m²", quantidade: 0, material: "SAC 350", peso: 0},
+    { modelo: "SHEET TH. 7,938 mm",  materialMod: "", descricao: "CHAPA 5/16\"", unidade: "m²", quantidade: 0, material: "ASTM A-36", peso: 0},
+    { modelo: "SHEET TH. 9,525 mm",  materialMod: "", descricao: "CHAPA 3/8\"", unidade: "m²", quantidade: 0, material: "ASTM A-36", peso: 0},
+    { modelo: "SHEET TH. 12,700 mm",  materialMod: "", descricao: "CHAPA 1/2\"", unidade: "m²", quantidade: 0, material: "ASTM A-36", peso: 0},
+    { modelo: "SHEET TH. 15,875 mm",  materialMod: "", descricao: "CHAPA 5/8\"", unidade: "m²", quantidade: 0, material: "ASTM A-36", peso: 0},
+    { modelo: "SHEET TH. 19,050 mm",  materialMod: "", descricao: "CHAPA 3/4\"", unidade: "m²", quantidade: 0, material: "ASTM A-36", peso: 0},
+    { modelo: "SHEET TH. 25,400 mm",  materialMod: "", descricao: "CHAPA 1\"", unidade: "m²", quantidade: 0, material: "ASTM A-36", peso: 0},
+    { modelo: "SHEET TH. 31,750 mm",  materialMod: "", descricao: "CHAPA 1.1/4\"", unidade: "m²", quantidade: 0, material: "ASTM A-36", peso: 0},
+    { modelo: "SHEET TH. 50,800 mm",  materialMod: "", descricao: "CHAPA 2\"", unidade: "m²", quantidade: 0, material: "ASTM A-36", peso: 0},
+    //PERFIL L
+    { modelo: "L 2 x 2 x 3/16",  materialMod: "", descricao: "L 2\" x 3/16\"", unidade: "m", quantidade: 0, material: "ASTM A-36", peso: 0},
+    { modelo: "L 2 x 2 x 1/4",  materialMod: "L 2\" x 1/4\"", descricao: "", unidade: "m", quantidade: 0, material: "ASTM A-36", peso: 0},
+    { modelo: "L 2 1/2 x 2 1/2 x 3/16",  materialMod: "", descricao: "L 2.1/2\" x 3/16\"", unidade: "m", quantidade: 0, material: "ASTM A-36", peso: 0},
+    { modelo: "L 3 x 3 x 3/16",  materialMod: "", descricao: "L 3\" x 3/16\"", unidade: "m", quantidade: 0, material: "ASTM A-36", peso: 0},
+    { modelo: "L 3 x 3 x 1/4",  materialMod: "", descricao: "L 3\" x 1/4\"", unidade: "m", quantidade: 0, material: "ASTM A-36", peso: 0},
+    { modelo: "L 4 x 4 x 5/16",  materialMod: "", descricao: "L 4\" x 5/16\"", unidade: "m", quantidade: 0, material: "ASTM A-36", peso: 0},
+    //PERFIL U
+    { modelo: "C 4x5.4",  materialMod: "", descricao: "PERFIL U 4\" - 1ªALMA", unidade: "m", quantidade: 0, material: "ASTM A-36", peso: 0},
+    { modelo: "C 6x8.2",  materialMod: "", descricao: "PERFIL U 6\" - 1ªALMA", unidade: "m", quantidade: 0, material: "ASTM A-36", peso: 0},
+    { modelo: "C 6 x 8,2",  materialMod: "", descricao: "PERFIL U 6\" - 1ªALMA", unidade: "m", quantidade: 0, material: "ASTM A-36", peso: 0},
+    { modelo: "C 8 x 11,5",  materialMod: "", descricao: "PERFIL U 8\" - 1ªALMA", unidade: "m", quantidade: 0, material: "ASTM A-36", peso: 0},
+    { modelo: "C 8x11.5",  materialMod: "", descricao: "PERFIL U 8\" - 1ªALMA", unidade: "m", quantidade: 0, material: "ASTM A-36", peso: 0},
+    //TUBOS
+    { modelo: "PIPE ØEXT. 33,400 mmx3,380 mm",  materialMod: "", descricao: "TUBO DN 1\" SCH40", unidade: "m", quantidade: 0, material: "ASTM A53 Gr.B", peso: 0},
+    { modelo: "PIPE ØEXT. 42,200 mmx3,560 mm",  materialMod: "", descricao: "TUBO DN 1.1/4\" SCH40", unidade: "m", quantidade: 0, material: "ASTM A53 Gr.B", peso: 0},
+    //PERFIL W
+    { modelo: "VIGA W 203,000 mmx102,000 mm",  materialMod: "", descricao: "W 200X19.3", unidade: "m", quantidade: 0, material: "ASTM A572 Gr. 50", peso: 0},
+    { modelo: "W150 x 22.5",  materialMod: "", descricao: "W 150x22.5", unidade: "m", quantidade: 0, material: "ASTM A572 Gr. 50", peso: 0},
+    { modelo: "W200 x 26.6",  materialMod: "", descricao: "W 200x26.6", unidade: "m", quantidade: 0, material: "ASTM A572 Gr. 50", peso: 0},
+    { modelo: "W200 x 46.1",  materialMod: "", descricao: "W 200x46.1", unidade: "m", quantidade: 0, material: "ASTM A572 Gr. 50", peso: 0},
+    { modelo: "W200 x 52",  materialMod: "", descricao: "W 200x52.0", unidade: "m", quantidade: 0, material: "ASTM A572 Gr. 50", peso: 0},
+    { modelo: "W250 x 32.7",  materialMod: "", descricao: "W 200x32.7", unidade: "m", quantidade: 0, material: "ASTM A572 Gr. 50", peso: 0},
+    //CURVAS
+    { modelo: "CURVA 90º DN 1\" SCH40 CFME ANSI B16.9 RC",  materialMod: "", descricao: "CURVA 90º DN 1\" SCH40 CFME ANSI B16.9 RC", unidade: "N", quantidade: 0, material: "ASTM A-36", peso: 0},
+    { modelo: "CURVA 90º DN 1.1/4\" SCH40 CFME ANSI B16.9 RC",  materialMod: "", descricao: "CURVA 90º DN 1.1/4\" SCH40 CFME ANSI B16.9 RC", unidade: "N", quantidade: 0, material: "ASTM A-36", peso: 0},
+    //{ modelo: "",  materialMod: "", descricao: "", unidade: "", quantidade: 0, material: "", peso: 0},
+  ];
+  
+  const findMaterial = (descricao) => {
+    let info = listMaterial.filter(obj => obj.modelo == descricao && obj.materialMod == "");
     return info[0];
-}
-
-const findMaterialEsp = (descricao, material) => {
-    let listMaterial = [
-        //CHAPAS MM
-        {modelo: "SHEET TH. 6,350 mm", materialMod: "USI SAC 350", descricao: "CHAPA 1/4\"", material: "SAC 350"},
-        //{modelo: "",  materialMod: "", descricao: "", material: ""},
-    ] 
+  };
+  
+  const findMaterialEsp = (descricao, material) => {
     let info = listMaterial.filter(obj => obj.modelo == descricao && obj.materialMod == material);
     return info[0];
-}
-
-module.exports = {findMaterial, findMaterialEsp}
+  };
+  
+  const findMaterialPos = (descricao, material) => {
+    let pos = listMaterial.findIndex(obj => obj.descricao == descricao && obj.material == material);
+    let info = listMaterial.filter(obj => obj.descricao == descricao && obj.material == material);
+    return [pos, info[0].descricao, info[0].unidade, info[0].quantidade, info[0].material, info[0].peso];
+  };
+  
+  module.exports = { findMaterial, findMaterialEsp, findMaterialPos };
