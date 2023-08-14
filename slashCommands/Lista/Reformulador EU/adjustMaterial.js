981f14891bf1aa38c2bfb293a372dee5afe4e363
Kzuy1 <leoczuy@gmail.com>
2023-08-14 09:20:08 -0300

    Update adjustMaterial.js

--- slashCommands/Lista/Reformulador EU/adjustMaterial.js
+++ slashCommands/Lista/Reformulador EU/adjustMaterial.js
@@ -1,6 +1,6 @@
 const adjustMaterial = (material) => {
     let nameMaterial = '';
-    let materialList = ["S235JR", "S275JR", "S3555JR", "AISI 304L", "AISI 316L", "PTFE", "COR-TEN B", "STRENX 700", "P265GH"];
+    let materialList = ["S235JR", "S275JR", "S3555JR", "S32205", "AISI 304L", "AISI 316L", "PTFE", "COR-TEN B", "STRENX 700", "P265GH"];
 
     for(c = 0; c < materialList.length; c++){
         if(material.includes(materialList[c])){
