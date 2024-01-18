d0b63c58c5b974894a908399f3ebc09da352c7f4
Kzuy1 <leoczuy@gmail.com>
2024-01-18 13:19:54 -0300

    Correct_error_in_L-Profile

--- slashCommands/Lista/Importa_Excel_Para_DWG.js
+++ slashCommands/Lista/Importa_Excel_Para_DWG.js
@@ -1,25 +1,25 @@
-const { ApplicationCommandOptionType } = require("discord.js");
-const https = require("https");
-const fs = require("fs");
-const axios = require("axios");
-const FormData = require("form-data");
+const { ApplicationCommandOptionType } = require('discord.js');
+const https = require('https');
+const fs = require('fs');
+const axios = require('axios');
+const FormData = require('form-data');
 
 module.exports = {
-	name: "importa_excel_para_dwg",
-	category: "Lista de Material",
-	description: "Este comando converte os dados da planilha para o DWG",
+	name: 'importa_excel_para_dwg',
+	category: 'Lista de Material',
+	description: 'Este comando converte os dados da planilha para o DWG',
 	ownerOnly: false,
 	options: [
 		{
-			name: "planilha",
-			description: "Anexe a Planilha",
+			name: 'planilha',
+			description: 'Anexe a Planilha',
 			type: ApplicationCommandOptionType.Attachment,
 			required: true
 		}
 	],
 
 	run: async (client, interaction) => {
-		const attachment = interaction.options.get("planilha");
+		const attachment = interaction.options.get('planilha');
 		const path = `${__dirname}/PlanilhasSaves/${attachment.attachment.name}`;
 
 		function download(){
@@ -27,11 +27,11 @@ module.exports = {
 				https.get(attachment.attachment.url,(res) => {
 					const filePath = fs.createWriteStream(path);
 					res.pipe(filePath);
-					filePath.on("finish",() => {
+					filePath.on('finish',() => {
 						filePath.close();
 						resolve();
 					});
-					filePath.on("error", (err) => {
+					filePath.on('error', (err) => {
 						console.log(err);
 						fs.unlink(path, () => reject(err));
 					});
@@ -44,20 +44,20 @@ module.exports = {
 			try {
 				const fileContent = fs.readFileSync(path);
 				const formData = new FormData();
-				formData.append("file", fileContent, path.match(/\/([^/]+)$/)[0]);
+				formData.append('file', fileContent, path.match(/\/([^/]+)$/)[0]);
                 
 				// Usar o await para esperar a resposta do servidor antes de prosseguir
-				const response = await axios.post("https://chloeape.discloud.app:443/upload", formData, {
+				const response = await axios.post('https://chloeape.discloud.app:443/upload', formData, {
 					headers: formData.getHeaders(), 
-					responseType: "arraybuffer",
+					responseType: 'arraybuffer',
 				});
                 
-				const filename = response.headers["content-disposition"].split("filename=")[1].replace(/"/g, "").trim();
+				const filename = response.headers['content-disposition'].split('filename=')[1].replace(/"/g, '').trim();
 				const responsePath = `${__dirname}/BlockList/${filename}`;
 				fs.writeFileSync(responsePath, response.data);
 				interaction.channel.send({content: `<@${interaction.user.id}>, aqui está os arquivos`, files: [responsePath]});
 			} catch (error) {
-				console.error("Erro ao enviar o arquivo:", error.message);
+				console.error('Erro ao enviar o arquivo:', error.message);
 			}
 		}
 		await apiRequest();
