faf5f1d3a1cf6b3b1f7bef7f00742d01da33fa8b
Kzuy1 <leoczuy@gmail.com>
2024-07-31 14:49:57 -0300

    add_ASTM-A53

--- slashCommands/Lista/Reformulador EU/adjustMaterial.js
+++ slashCommands/Lista/Reformulador EU/adjustMaterial.js
@@ -1,6 +1,7 @@
 const materialList = [
   {pos: '', nome: 'S235JR', densidade: 7.850, modeloMod: 'S235JR'},
   {pos: '', nome: 'ASTM A36', densidade: 7.850, modeloMod: 'ASTM A36'},
+  {pos: '', nome: 'ASTM A53 Gr.B', densidade: 7.850, modeloMod: 'ASTM A53 Gr.B'},
   {pos: '', nome: 'S275JR', densidade: 7.850, modeloMod: 'S275JR'},
   {pos: '', nome: 'S355JR', densidade: 7.850, modeloMod: 'S355JR'},
   {pos: '', nome: 'S3555JR', densidade: 7.850, modeloMod: 'S3555JR'},
